cc360c24b9cc30963d038ee437fcb943eb842850
halin_hi <halin130@naver.com>
2022-05-06 16:56:46 +0900

    map 현위치 표시

--- app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
@@ -2,19 +2,33 @@ package com.tukorea.siheunghere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
+import com.tukorea.siheunghere.VariableOnMap as VM
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
+
+        // onMapReady() 콜백 메서드가 호출
+        mapView.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, VM.LOCATION_PERMISSTION_REQUEST_CODE)
     }
 
     override fun onStart() {
@@ -52,7 +66,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.onLowMemory()
     }
 
-    override fun onMapReady(p0: NaverMap) {
-        TODO("Not yet implemented")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        //현위치 요청 결과 코드
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            else {                             // 권한 승인됨 -> 지도 TrackingMode를 Follow로 설정
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    //NaverMap 객체가 준비되면 호출되는 함수
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        val marker = Marker()
+        marker.position = LatLng(37.56683771710133, 126.97864942520158)
+        marker.icon = OverlayImage.fromResource(R.drawable.map_badminton)
+        marker.width = VM.MARKER_SIZE
+        marker.height = VM.MARKER_SIZE
+        marker.map = naverMap
     }
 }
