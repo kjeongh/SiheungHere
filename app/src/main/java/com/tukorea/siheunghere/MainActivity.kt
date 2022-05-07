08146370c6282851524b1d23717d8a78bd2df9bc
halin130 <halin130@naver.com>
2022-05-08 07:59:17 +0900

    노트북과 데스크탑 병합

--- app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
@@ -2,25 +2,12 @@ package com.tukorea.siheunghere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-=======
-import android.view.View
-import androidx.appcompat.app.AlertDialog
-import com.naver.maps.geometry.LatLng
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
-import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
-<<<<<<< HEAD
-import com.naver.maps.map.util.FusedLocationSource
-
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mapView: MapView
-    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
-    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
-    private lateinit var naverMap: NaverMap
-=======
+import android.view.View
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.Overlay
@@ -46,21 +33,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mapView: MapView
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
-    private lateinit var dialogView : View
-    val infoWindow = InfoWindow()
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
-
-        mapView = findViewById(R.id.map_view)
-        mapView.onCreate(savedInstanceState)
-
-        mapView.getMapAsync(this)
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
-=======
         mapView = findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
 
@@ -69,69 +45,37 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         //현위치 받아오기
         locationSource = FusedLocationSource(this, VM.LOCATION_PERMISSTION_REQUEST_CODE)
-
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     }
 
     override fun onStart() {
         super.onStart()
         mapView.onStart()
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     override fun onResume() {
         super.onResume()
         mapView.onResume()
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     override fun onPause() {
         super.onPause()
         mapView.onPause()
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     override fun onStop() {
         super.onStop()
         mapView.onStop()
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
     }
 
-<<<<<<< HEAD
-    override fun onMapReady(p0: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-=======
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         //현위치 요청 결과 코드
         if (locationSource.onRequestPermissionsResult(requestCode, permissions,
@@ -171,6 +115,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         marker.map = naverMap
 
         marker.onClickListener = listener
->>>>>>> 19a0f867c256056dc710305dfae221cf8c04d61f
     }
 }
