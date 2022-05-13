b0225f8b922c779c4f1d55fc64f7dd09311d046c
halin130 <halin130@naver.com>
2022-05-14 08:51:03 +0900

    #10 geocode 관련 파일 추가

---
+++ app/src/main/java/com/tukorea/siheunghere/Addresses.kt
@@ -0,0 +1,11 @@
+package com.tukorea.siheunghere
+
+data class Addresses (
+    val addressElements: List<AddressElement>,
+    val distance: Double,
+    val englishAddress: String,
+    val jibunAddress: String,
+    val roadAddress: String,
+    val x: String,
+    val y: String
+)
---
+++ app/src/main/java/com/tukorea/siheunghere/GeoResponse.kt
@@ -0,0 +1,9 @@
+package com.tukorea.siheunghere
+
+data class GeoResponse(
+    
+    val addresses: List<Addresses>,
+    val errorMessage: String,
+    val meta: Meta,
+    val status: String
+)
