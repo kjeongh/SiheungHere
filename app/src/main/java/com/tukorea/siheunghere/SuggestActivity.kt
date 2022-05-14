2e0a0493bfb0bb9821fced9a7c884ab73445385e
jh.kim <wellsy001012@gmail.com>
2022-05-14 20:46:05 +0900

    #30 파일명 변경

--- app/src/main/java/com/tukorea/siheunghere/SuggestActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/SuggestActivity.kt
@@ -9,7 +9,7 @@ class SuggestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_suggest)
+        setContentView(R.layout.suggest_activity)
 
         //다시 건의글 버튼 누르면 홈화면으로 돌아감
         title_suggestBtn.setOnClickListener() {
