20b4cff3a1d1a148f6b6ba1f06861b818bd25aec
p-chanmin <pc_m111@naver.com>
2022-05-17 00:29:56 +0900

    #41 건의 내용, 비밀번호 글자수 제한

--- app/src/main/java/com/tukorea/siheunghere/SuggestActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/SuggestActivity.kt
@@ -1,9 +1,14 @@
 package com.tukorea.siheunghere
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.main_title.*
 import kotlinx.android.synthetic.main.suggest_activity.*
+import kotlinx.android.synthetic.main.suggest_activity.view.*
+import okio.utf8Size
 
 
 class SuggestActivity : AppCompatActivity() {
@@ -12,9 +17,26 @@ class SuggestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.suggest_activity)
 
+
         //다시 건의글 버튼 누르면 홈화면으로 돌아감
         title_suggestBtn.setOnClickListener() {
             finish()
+        }
+
+        // 자원 아이콘 선택 다이얼로그
+        iconEdit.setOnClickListener {
+
+        }
+        mapEdit.setOnClickListener {
+
+        }
+        // 건의 내용 제한 표시 <100자>
+        memoEdit.addTextChangedListener {
+            memoLimitEdit.text = memoEdit.text.toString().utf8Size().toString() + "/100"
+        }
+        // 비밀번호 제한 표시 <20자>
+        pwEdit.addTextChangedListener {
+            pwLimitEdit.text = pwEdit.text.toString().utf8Size().toString() + "/20"
         }
 
         val suggestList = ArrayList<SuggestData>(5)
@@ -23,8 +45,6 @@ class SuggestActivity : AppCompatActivity() {
         suggestList.add(SuggestData(12,"경기도 시흥시 새재로 19윤성빌딩 5층 502호",1230))
         suggestList.add(SuggestData(13,"경기도 시흥시 새재로 19윤성빌딩 5층 502호",4320))
         suggestList.add(SuggestData(14,"경기도 시흥시 새재로 19윤성빌딩 5층 502호",1520))
-
-
 
 
         val suggestListAdapter = SuggestListViewAdapter(this, suggestList)
