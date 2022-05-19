a72f449dc99952ab35d553f56728cec580659686
halin130 <halin130@naver.com>
2022-05-19 12:34:13 +0900

    #39 검색할 반지름 거리 설정 버튼 추가

--- app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
@@ -40,10 +40,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     // - 필터링으로 걸러지는 자원 제외, marker를 다 null로 설정(안보이게)
 
     private lateinit var naverMap: NaverMap
+    private lateinit var uiSettings : UiSettings                // 지도 UI 세팅 객체
     private lateinit var locationSource: FusedLocationSource
-    private lateinit var circle: CircleOverlay                  //현위치 or 지도중심점(이 위치에서 재검색 할 경우) 중심으로 그려질 원(거리 계산)
-    private lateinit var cameraPos: CameraPosition              //지도 중심점 위치(LatLng)
-    private lateinit var uiSettings : UiSettings
+    private lateinit var circle: CircleOverlay                  // 현위치 or 지도중심점(이 위치에서 재검색 할 경우) 중심으로 그려질 원(거리 계산)
+    private lateinit var cameraPos: CameraPosition              // 지도 중심점 위치(LatLng)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
@@ -55,13 +56,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             ?: MapFragment.newInstance().also {
                 fm.beginTransaction().add(R.id.map, it).commit()
             }
+
         //navermap 준비되면 호출되는 함수
         mapFragment.getMapAsync(this)
         //현위치 받아오기
         locationSource = FusedLocationSource(this, VM.LOCATION_PERMISSTION_REQUEST_CODE)
 
         // 공유자원 database
-        changeAddresstoCoord()
+        //changeAddresstoCoord()
 
         //타이틀바 건의글 게시판 이동 버튼
         title_suggestBtn.setOnClickListener() {
@@ -91,15 +93,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         ResearchBtn.setOnClickListener {
             cameraPos = naverMap.cameraPosition
             makeMarker(cameraPos.target, R.drawable.map_cafe)
+            makeCircle(cameraPos.target, 1500.0)
         }
+
+
     }
     override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         //현위치 요청 결과 코드
-        if (locationSource.onRequestPermissionsResult(
-                requestCode, permissions,
-                grantResults
-            )
-        ) {
+        if (locationSource.onRequestPermissionsResult( requestCode, permissions,grantResults )) {
             if (!locationSource.isActivated) { // 권한 거부됨 -> 지도 TrackingMode를 None으로 설정
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             } else {                             // 권한 승인됨 -> 지도 TrackingMode를 Follow로 설정
@@ -155,10 +156,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun changeAddresstoCoord(){
         val db = Firebase.firestore
         val retrofit = RetrofitBuilder().retrofit
+        //db에 있는 모든 document 가져오기
         db.collection("shared").get().addOnSuccessListener { result ->
             for (document in result) {
                 var address = document.get("address")
-                //각 document는 id를 불러올 수 있다. 이 id를 활용해 주소 -> 위도, 경도 변환한 것을 데이터베이스에 넣자
+                //각 document의 주소를 불러와 searchAddress 함수를 호출해 주소 변환
                 var docId = document.id
                 retrofit.create(NaverMapApi::class.java).searchAddress(address as String)
                     .enqueue(object : Callback<GeoResponse> {
@@ -177,6 +179,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                                 "latitude" to latitude,
                                 "longtitude" to longtitude
                             )
+                            //위도와 경도까지 추가된 document를 덮어쓰기
                             db.collection("shared").document("${docId}")
                                 .set(shared)
                                 .addOnSuccessListener { Log.d("db", "DocumentSnapshot successfully written!") }
