f38a6525399ee9a9361912e3fecfde55c4545b7f
halin_hi <halin130@naver.com>
2022-05-22 20:51:55 +0900

    #39 지도 검색 기능 구현

--- app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
@@ -4,6 +4,7 @@ package com.tukorea.siheunghere
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.*
 import com.google.firebase.firestore.ktx.firestore
@@ -16,29 +17,34 @@ import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
 import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.main_icon_scroll.*
 import kotlinx.android.synthetic.main.main_slidingdrawer.*
 import kotlinx.android.synthetic.main.main_title.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.lang.reflect.Field
+import kotlin.math.ln
 import com.tukorea.siheunghere.VariableOnMap as VM
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     // < ----- 구현해야할 것 ----- >
     // < test >
-    // 1. 자원을 저장할 데이터 객체(주소, 종류, 전화번호, 사진)
+    // 1. 자원을 저장할 데이터 객체(주소, 종류, 전화번호, 사진) -> 완료
     // - 각 marker 아이콘 설정
 
     // 2. 현위치와 거리계산
-    // - 위치 중심으로 그려진 원 안에 있는 자원들만 데이터베이스에서 읽어오기
-    // - 이 위치에서 재검색 누르면 선택한 자원 필터는 초기화
+    // - 위치 중심으로 그려진 원 안에 있는 자원들만 데이터베이스에서 읽어오기 -> 완료
+    // - 이 위치에서 재검색 누르면 선택한 자원 필터는 초기화 -> 완료
 
     // 3. 그 외의 것
-    // - 자원 필터링 -> 위에서 거리계산된 것들 중에서 선택한 자원만 보여주면 될듯
-    // - 마커 생성 함수가 return 하는 marker는 각 객체에 변수로 저장
-    // - 필터링으로 걸러지는 자원 제외, marker를 다 null로 설정(안보이게)
+    // - 자원 필터링 -> 위에서 거리계산된 것들 중에서 선택한 자원만 보여주면 될듯 -> test 전
+    // - 마커 생성 함수가 return 하는 marker는 각 객체에 변수로 저장 -> 완료
+    // - 필터링으로 걸러지는 자원 제외, marker를 다 null로 설정(안보이게) -> 완료
+
+    // 4. 마커 다이얼로그
+    // - firebase storage에서 사진 불러오기
+    // dialog에 정보 넣기
 
     // 지도 관련 변수
     private lateinit var naverMap: NaverMap                     // 지도 객체
@@ -53,10 +59,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var southLatitude: Double = 0.0
     private var eastLongitude: Double = 0.0
     private var westLongitude: Double = 0.0
+    // 사용자가 설정하는 검색할 반경 거리(default: 1km)
+    private var distance: Double = VM.DISTANCE_1
 
     //데이터베이스 관련 변수
     private lateinit var db: FirebaseFirestore
     private lateinit var sharedRef: CollectionReference
+    private var sharedList = mutableListOf<SharedResource>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,17 +79,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 fm.beginTransaction().add(R.id.map, it).commit()
             }
 
+        //레이아웃 관련 변수 설정
+        val scrollIcons = arrayOf(icon_badminton, icon_baseball, icon_cafe, icon_classroom, icon_park, icon_cooling_center, icon_experience,
+        icon_futsal, icon_gallery, icon_livingsport, icon_meeting, icon_parking,
+           icon_practice_room, icon_soccer_field, icon_theater, icon_toilet, icon_wifi)
+
         //navermap 준비되면 호출되는 함수
         mapFragment.getMapAsync(this)
+
         //현위치 받아오기
         locationSource = FusedLocationSource(this, VM.LOCATION_PERMISSTION_REQUEST_CODE)
 
+        // 데이터베이스 관련 변수 설정
         db = Firebase.firestore
         sharedRef = db.collection("shared")
 
 
         // 주소를 좌표로 변환해서 database에 넣기
-        changeAddresstoCoord()
+        //changeAddresstoCoord()
 
         //타이틀바 건의글 게시판 이동 버튼
         title_suggestBtn.setOnClickListener() {
@@ -110,10 +127,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         ResearchBtn.setOnClickListener {
             // 지도 중심 좌표 get & 반경 그리기
             cameraPos = naverMap.cameraPosition
-            makeCircle(cameraPos.target, VM.DISTANCE_3)
+            makeCircle(cameraPos.target, distance)
 
+            // 위도 또는 경도 범위에 해당하는 데이터만 담기
             var latResult = mutableSetOf<DocumentSnapshot>()
             var lngResult = mutableSetOf<DocumentSnapshot>()
+
+            // 검색하기 전 리스트에 있던 자원 지우기 & 초기화
+            if(sharedList != null){
+                for (sharedresource in sharedList){
+                    sharedresource.marker?.map = null
+                }
+                sharedList = mutableListOf()
+            }
 
             // 범위 안에 있는 데이터 찾는 query(동서남북 경계 이용)
             sharedRef.whereGreaterThan("latitude", southLatitude).whereLessThan("latitude", northLatitude).get().addOnSuccessListener { documents ->
@@ -121,46 +147,45 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     latResult.add(document)
                 }
                 if(lngResult != null){
-                    var wholeResult = latResult.intersect(lngResult)
-                    for (doc in wholeResult) {
-                        var lat = doc.get("latitude")
-                        var lng = doc.get("longitude")
-                        var kind = "map_" + doc.get("kind").toString()
-                        var icon = resources.getIdentifier(kind, "drawable", packageName)
-                        makeMarker(LatLng(lat as Double, lng as Double), icon)
-                    }
+                    makeResultList(latResult, lngResult)
                 }
             }
-
-
             sharedRef.whereGreaterThan("longitude", westLongitude).whereLessThan("longitude", eastLongitude).get().addOnSuccessListener { documents ->
                 for (document in documents) {
                     lngResult.add(document)
                 }
                 if(latResult != null){
-                    var wholeResult = latResult.intersect(lngResult)
-                    for (doc in wholeResult) {
-                        var lat = doc.get("latitude")
-                        var lng = doc.get("longitude")
-                        var kind = "map_" + doc.get("kind").toString()
-                        var icon = resources.getIdentifier(kind, "drawable", packageName)
-                        makeMarker(LatLng(lat as Double, lng as Double), icon)
-                    }
+                    makeResultList(latResult, lngResult)
                 }
             }
         }
 
         // 검색할 반지름 거리 설정 버튼
         DistBtn1.setOnClickListener {
-            makeCircle(cameraPos.target, VM.DISTANCE_1)
+            distance = VM.DISTANCE_1
         }
         DistBtn2.setOnClickListener {
-            makeCircle(cameraPos.target,  VM.DISTANCE_2)
+            distance = VM.DISTANCE_2
         }
         DistBtn3.setOnClickListener {
-            makeCircle(cameraPos.target,  VM.DISTANCE_3)
+            distance = VM.DISTANCE_3
         }
 
+        // 아이콘 버튼(필터링 기능)
+        for(i in scrollIcons.indices) {
+            scrollIcons[i].setOnClickListener {
+                var iconName = scrollIcons[i].toString().split("/")[1].replace("}", "")
+                iconName = iconName.replace("icon_", "")
+                for(sharedresource in sharedList) {
+                    if (sharedresource.kind == iconName) {
+                        sharedresource.marker?.map = naverMap
+                    }
+                    else {
+                        sharedresource.marker?.map = null
+                    }
+                }
+            }
+        }
     }
     override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         //현위치 요청 결과 코드
@@ -221,8 +246,23 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         westLongitude = boundary.westLongitude
 
         // 경계 위도와 경도 받기 -> 완료
-        // 경계에 포함되어 있는 document만 불러오기
+        // 경계에 포함되어 있는 document만 불러오기 -> 완료
         // 그것만 marker 띄우기
+    }
+
+    private fun makeResultList(latResult: MutableSet<DocumentSnapshot>, lngResult: MutableSet<DocumentSnapshot>){
+        var wholeResult = latResult.intersect(lngResult)
+        for (doc in wholeResult) {
+            var lat = doc.get("latitude").toString().toDouble()
+            var lng = doc.get("longitude").toString().toDouble()
+            var tel = doc.get("tel").toString()
+            var kind = doc.get("kind").toString()
+            var name = doc.get("name").toString()
+            var icon = resources.getIdentifier("map_" + kind, "drawable", packageName)
+            var sharedItem = SharedResource(lat, lng, tel, kind, name)
+            sharedItem.marker = makeMarker(LatLng(lat, lng), icon)
+            sharedList.add(sharedItem)
+        }
     }
 
     //db에 있는 공유자원의 주소를 위도, 경도로 변환해 db에 넣음(데이터 준비, 앱 출시할 때는 없어질 코드)
@@ -263,6 +303,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }.addOnFailureListener { exception -> Log.w(VM.TAG, "Error getting documents: ", exception) }
     }
+
 
 /*    private fun searchAddress(query: String) {
         val retrofit = RetrofitBuilder().retrofit
--- app/src/main/java/com/tukorea/siheunghere/VariableOnMap.kt
+++ app/src/main/java/com/tukorea/siheunghere/VariableOnMap.kt
@@ -5,8 +5,8 @@ object VariableOnMap {
     const val MARKER_SIZE = 120                                 // Marker 설정 크기
     const val LOCATION_PERMISSTION_REQUEST_CODE = 1000          // 현위치 요청 코드
     const val MIN_ZOOM = 8.0                                    // 줌 아웃 최대치
-    const val DISTANCE_1 = 1000.0                               // 검색할 반지름 거리 값(m)
-    const val DISTANCE_2 = 1500.0
-    const val DISTANCE_3 = 2000.0
+    const val DISTANCE_1 = 500.0                                // 검색할 반지름 거리 값(m)
+    const val DISTANCE_2 = 1000.0
+    const val DISTANCE_3 = 1500.0
     const val TAG = "Firestore"
 }
