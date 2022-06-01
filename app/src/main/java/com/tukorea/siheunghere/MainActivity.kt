d4d6f04eb26f083d207ad98cb645ccd30cc32664
p-chanmin <pc_m111@naver.com>
2022-06-01 23:17:24 +0900

    #51 슬라이딩 드로어 개선

--- app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
+++ app/src/main/java/com/tukorea/siheunghere/MainActivity.kt
@@ -194,6 +194,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                     }
                     if (lngResult != null) {
                         makeResultList(latResult, lngResult)
+                        // 슬라이딩 드로어 리스트 어댑터 갱신
+                        mapAdaptor.setList(sharedList)
                     }
                 }
             sharedRef.whereGreaterThan("longitude", westLongitude)
